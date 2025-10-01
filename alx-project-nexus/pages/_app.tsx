4e55b0356828cb586d2302d6e70e8e8a9a70cc53
Collins Boakye <colly0087@gmail.com>
2025-10-01 07:52:49 -0700

    update: Errorboundary added

--- alx-project-nexus/pages/_app.tsx
+++ alx-project-nexus/pages/_app.tsx
@@ -2,14 +2,17 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layouts/Layout";
 import { ProductProvider } from "@/context/ProductContext";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ProductProvider>
-      <Layout>
-        <Component {...pageProps} />;
-      </Layout>
-    </ProductProvider>
+    <ErrorBoundary>
+      <ProductProvider>
+        <Layout>
+          <Component {...pageProps} />;
+        </Layout>
+      </ProductProvider>
+    </ErrorBoundary>
   )
 
 }
