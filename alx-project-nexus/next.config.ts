f0c4f51e24db80e6e7629e359290ae7b3248d7dd
Collins Boakye <colly0087@gmail.com>
2025-10-01 07:41:01 -0700

    feat(pwa): add progressive web app support with manifest and service worker

--- alx-project-nexus/next.config.ts
+++ alx-project-nexus/next.config.ts
@@ -1,4 +1,11 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+/* @type {import('next').NextConfig} */
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,11 +13,21 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      }
-    ]
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+    ],
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+});
