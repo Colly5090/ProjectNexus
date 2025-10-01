9ef224bb7ee69abea83ccb1e2d3aab74a2bb1fc1
Collins Boakye <colly0087@gmail.com>
2025-10-01 08:32:22 -0700

    update: favicon, title and meta added

--- alx-project-nexus/pages/_app.tsx
+++ alx-project-nexus/pages/_app.tsx
@@ -3,16 +3,26 @@ import type { AppProps } from "next/app";
 import Layout from "@/components/layouts/Layout";
 import { ProductProvider } from "@/context/ProductContext";
 import ErrorBoundary from "@/components/ErrorBoundary";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary>
-      <ProductProvider>
-        <Layout>
-          <Component {...pageProps} />;
-        </Layout>
-      </ProductProvider>
-    </ErrorBoundary>
+    <>
+      <Head>
+        <title>NexusShop - Product Catalog Store</title>
+        <meta name="description" content="Shop the latest electronics, fashion, and furniture with amazing discounts." />
+        <link rel="icon" href="/assets/icons/brand.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/apple-icon-152x152.png" />
+      </Head>
+      <ErrorBoundary>
+        <ProductProvider>
+          <Layout>
+            <Component {...pageProps} />;
+          </Layout>
+        </ProductProvider>
+      </ErrorBoundary>
+    </>
+
   )
 
 }
